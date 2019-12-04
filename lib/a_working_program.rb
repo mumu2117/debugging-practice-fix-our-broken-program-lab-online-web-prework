e9bf5b46d27fe2be07f09c37ca3ed22d2f9f251c
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-04 22:29:23 +0000

    Automatically backed up by Learn

--- lib/a_working_program.rb
+++ lib/a_working_program.rb
@@ -1,1 +1,7 @@
 puts Hello World! 
+
+puts "Hello World!" # Correct this to "Hello World!"
+
+
+# Your entire program should read:	# Your entire program should read:
+# puts "Hello World!"	# puts "Hello World!"
